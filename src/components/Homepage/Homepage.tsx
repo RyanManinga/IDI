f74e2b27d1de32ac5f8b4d07975caaaf28a7225d
RyanManinga <ryanmaninga@gmail.com>
2024-07-20 20:44:08 +0300

    First vite upload

---
+++ src/components/Homepage/Homepage.tsx
@@ -0,0 +1,177 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import resets from '../_resets.module.css';
+import classes from './Homepage.module.css';
+import { Rectangle2Icon2 } from './Rectangle2Icon2.js';
+import { Rectangle2Icon3 } from './Rectangle2Icon3.js';
+import { Rectangle2Icon4 } from './Rectangle2Icon4.js';
+import { Rectangle2Icon5 } from './Rectangle2Icon5.js';
+import { Rectangle2Icon6 } from './Rectangle2Icon6.js';
+import { Rectangle2Icon7 } from './Rectangle2Icon7.js';
+import { Rectangle2Icon8 } from './Rectangle2Icon8.js';
+import { Rectangle2Icon9 } from './Rectangle2Icon9.js';
+import { Rectangle2Icon10 } from './Rectangle2Icon10.js';
+import { Rectangle2Icon11 } from './Rectangle2Icon11.js';
+import { Rectangle2Icon12 } from './Rectangle2Icon12.js';
+import { Rectangle2Icon13 } from './Rectangle2Icon13.js';
+import { Rectangle2Icon14 } from './Rectangle2Icon14.js';
+import { Rectangle2Icon15 } from './Rectangle2Icon15.js';
+import { Rectangle2Icon16 } from './Rectangle2Icon16.js';
+import { Rectangle2Icon } from './Rectangle2Icon.js';
+
+interface Props {
+  className?: string;
+}
+/* @figmaId 1:2 */
+export const Homepage: FC<Props> = memo(function Homepage(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <div className={classes.header}>
+        <div className={classes.frame3}>
+          <div className={classes.logo}>Logo</div>
+          <div className={classes.frame1}>
+            <div className={classes.home}>Home</div>
+            <div className={classes.services}>Services</div>
+            <div className={classes.projects}>Projects</div>
+            <div className={classes.about}>About</div>
+            <div className={classes.blogs}>Blogs</div>
+          </div>
+        </div>
+        <div className={classes.frame2}>
+          <div className={classes.letsTalk}>Lets Talk</div>
+        </div>
+      </div>
+      <div className={classes.banner}>
+        <div className={classes.frame8}>
+          <div className={classes.frame4}>
+            <div className={classes.helloIMCanCreativeDesignerInTu}>
+              <p className={classes.labelWrapper}>
+                <span className={classes.label}>Hello, </span>
+                <span className={classes.label2}>I’m Can.</span>
+                <span className={classes.label3}> </span>
+                <span className={classes.label4}>Creative Designer</span>
+                <span className={classes.label5}> in Turkey.</span>
+              </p>
+            </div>
+            <div className={classes.loremIpsumDolorSitAmetConsecte}>
+              Lorem ipsum dolor sit amet consectetur. At libero sit nec tincidunt eu at semper. Pharetra duis ipsum
+              laoreet amet sem imperdiet cras feugiat. Lacinia feugiat duis pellentesque et nec ipsum est.
+            </div>
+          </div>
+          <div className={classes.frame7}>
+            <div className={classes.frame5}>
+              <div className={classes.getInTouch}>Get in Touch</div>
+            </div>
+            <div className={classes.frame6}>
+              <div className={classes.viewAllWorks}>View All Works</div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.rectangle1}></div>
+      </div>
+      <div className={classes.info}>
+        <div className={classes.frame19}>
+          <div className={classes.frame10}>
+            <div className={classes.graphicDesigner}>Graphic Designer</div>
+            <div className={classes.rectangle2}>
+              <Rectangle2Icon className={classes.icon} />
+            </div>
+          </div>
+          <div className={classes.frame11}>
+            <div className={classes.graphicDesigner2}>Graphic Designer</div>
+            <div className={classes.rectangle22}>
+              <Rectangle2Icon2 className={classes.icon2} />
+            </div>
+          </div>
+          <div className={classes.frame12}>
+            <div className={classes.graphicDesigner3}>Graphic Designer</div>
+            <div className={classes.rectangle23}>
+              <Rectangle2Icon3 className={classes.icon3} />
+            </div>
+          </div>
+          <div className={classes.frame13}>
+            <div className={classes.graphicDesigner4}>Graphic Designer</div>
+            <div className={classes.rectangle24}>
+              <Rectangle2Icon4 className={classes.icon4} />
+            </div>
+          </div>
+          <div className={classes.frame14}>
+            <div className={classes.graphicDesigner5}>Graphic Designer</div>
+            <div className={classes.rectangle25}>
+              <Rectangle2Icon5 className={classes.icon5} />
+            </div>
+          </div>
+          <div className={classes.frame15}>
+            <div className={classes.graphicDesigner6}>Graphic Designer</div>
+            <div className={classes.rectangle26}>
+              <Rectangle2Icon6 className={classes.icon6} />
+            </div>
+          </div>
+          <div className={classes.frame16}>
+            <div className={classes.graphicDesigner7}>Graphic Designer</div>
+            <div className={classes.rectangle27}>
+              <Rectangle2Icon7 className={classes.icon7} />
+            </div>
+          </div>
+          <div className={classes.frame17}>
+            <div className={classes.graphicDesigner8}>Graphic Designer</div>
+            <div className={classes.rectangle28}>
+              <Rectangle2Icon8 className={classes.icon8} />
+            </div>
+          </div>
+        </div>
+        <div className={classes.frame18}>
+          <div className={classes.frame102}>
+            <div className={classes.graphicDesigner9}>Graphic Designer</div>
+            <div className={classes.rectangle29}>
+              <Rectangle2Icon9 className={classes.icon9} />
+            </div>
+          </div>
+          <div className={classes.frame112}>
+            <div className={classes.graphicDesigner10}>Graphic Designer</div>
+            <div className={classes.rectangle210}>
+              <Rectangle2Icon10 className={classes.icon10} />
+            </div>
+          </div>
+          <div className={classes.frame122}>
+            <div className={classes.graphicDesigner11}>Graphic Designer</div>
+            <div className={classes.rectangle211}>
+              <Rectangle2Icon11 className={classes.icon11} />
+            </div>
+          </div>
+          <div className={classes.frame132}>
+            <div className={classes.graphicDesigner12}>Graphic Designer</div>
+            <div className={classes.rectangle212}>
+              <Rectangle2Icon12 className={classes.icon12} />
+            </div>
+          </div>
+          <div className={classes.frame142}>
+            <div className={classes.graphicDesigner13}>Graphic Designer</div>
+            <div className={classes.rectangle213}>
+              <Rectangle2Icon13 className={classes.icon13} />
+            </div>
+          </div>
+          <div className={classes.frame152}>
+            <div className={classes.graphicDesigner14}>Graphic Designer</div>
+            <div className={classes.rectangle214}>
+              <Rectangle2Icon14 className={classes.icon14} />
+            </div>
+          </div>
+          <div className={classes.frame162}>
+            <div className={classes.graphicDesigner15}>Graphic Designer</div>
+            <div className={classes.rectangle215}>
+              <Rectangle2Icon15 className={classes.icon15} />
+            </div>
+          </div>
+          <div className={classes.frame172}>
+            <div className={classes.graphicDesigner16}>Graphic Designer</div>
+            <div className={classes.rectangle216}>
+              <Rectangle2Icon16 className={classes.icon16} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
---
+++ src/components/Homepage/Rectangle2Icon5.tsx
@@ -0,0 +1,26 @@
+import { memo, SVGProps } from 'react';
+
+const Rectangle2Icon5 = (props: SVGProps<SVGSVGElement>) => (
+  <svg preserveAspectRatio='none' viewBox='0 0 26 26' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <path
+      d='M10.8787 2.12136C12.0502 0.949782 13.9497 0.949784 15.1213 2.12136L23.8786 10.8787C25.0502 12.0503 25.0502 13.9498 23.8786 15.1213L15.1213 23.8787C13.9497 25.0503 12.0502 25.0503 10.8787 23.8787L2.12132 15.1213C0.949749 13.9498 0.949751 12.0503 2.12132 10.8787L10.8787 2.12136Z'
+      fill='url(#paint0_linear_104_6)'
+    />
+    <defs>
+      <linearGradient
+        id='paint0_linear_104_6'
+        x1={29.5}
+        y1={-14.5}
+        x2={-9.00002}
+        y2={46.5}
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#FF0000' />
+        <stop offset={1} stopColor='#9D02FC' />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+
+const Memo = memo(Rectangle2Icon5);
+export { Memo as Rectangle2Icon5 };
